3ad22582864d80c030a5661115ae770e242095e3
SharedOrdaz <sharedordaz@live.com.mx>
2025-06-05 00:38:45 -0600

    Models

---
+++ Models/Activity.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+namespace Backend.Models
+{
+
+    public class Activity
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        
+
+        public Activity(string id, string Name, string Description)
+        {
+            Id = id;
+            Name = Name;
+            Description = Description;
+            // Here you would typically save the user to a database
+            // For this example, we are just creating the object
+        }
+    }
+}
+    
