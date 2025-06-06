ddd919152c2c12528cc2d8c98e72ff44c5b1cb57
SharedOrdaz <sharedordaz@live.com.mx>
2025-06-05 18:08:27 -0600

    UserController

--- obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ad22582864d80c030a5661115ae770e242095e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27ac8a4faf452e3101ae72e5b3b525f82ddb7925")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
