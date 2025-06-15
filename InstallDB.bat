9d454e97a1f5b140b81ccd3431a8c4d02c1ee729
SharedOrdaz <sharedordaz@live.com.mx>
2025-06-14 18:55:09 -0600

    The script

--- InstallDB.bat
+++ InstallDB.bat
@@ -52,11 +52,17 @@ set DB_PASSWORD=fitpassword
 set DB_NAME=FitTrack_Database
 set SQL_FILE=db\database.sql
 
+
 :: Create the user and grant privileges
 echo Creating MySQL user and granting privileges...
 echo CREATE USER IF NOT EXISTS '%DB_USER%'@'localhost' IDENTIFIED BY '%DB_PASSWORD%'; GRANT ALL PRIVILEGES ON %DB_NAME%.* TO '%DB_USER%'@'localhost'; FLUSH PRIVILEGES; > create_user.sql
 
 mysql -u %DB_ROOT_USER%  < create_user.sql
+if %errorlevel% neq 0 (
+    echo Error: No se pudo crear el usuario de MySQL o asignar privilegios.
+    pause
+    exit /b
+)
 
 del create_user.sql
 
@@ -64,13 +70,13 @@ del create_user.sql
 :: Run the SQL script as the new user
 
 mysql -h %DB_HOST% -u %DB_USER% -p%DB_PASSWORD% < %SQL_FILE%
-
-if !%SQL_FILE%{
-    echo Error: No se ejecuto el script de la base de datos.
+if %errorlevel% neq 0 (
+    echo Error: No se pudo ejecutar el script de la base de datos.
     pause
     exit /b
-    }
-   
+)
+
+
 
 echo Database script executed successfully.
 pause
