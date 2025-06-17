fbedaa0ad2de64a8782b7bf3b404ea25e24b1d52
ijromerob <ijromerob@gmail.com>
2025-06-16 22:03:17 -0600

    fixing post for activities

--- Models/Activity.cs
+++ Models/Activity.cs
@@ -3,26 +3,29 @@ using System.Text.Json.Serialization;
 namespace Backend.Models
 {
 
-    public class Activity
+  public class Activity
+  {
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(100)]
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+
+    public ICollection<Routine> Routines { get; set; } = new List<Routine>();
+    public Activity(int id, string name, string description)
     {
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        [MaxLength(100)]
-        public string name { get; set; }
-
-        public string description { get; set; }
-
-        
-        public ICollection<Routine> Routines { get; set; } = new List<Routine>();
-        public Activity(int id, string name, string description)
-        {
-            Id = id;
-            name = name;
-            description = description;
-            // Here you would typically save the user to a database
-            // For this example, we are just creating the object
-        }
+      Id = id;
+      Name = name;
+      Description = description;
+      // Here you would typically save the user to a database
+      // For this example, we are just creating the object
     }
+
+    public Activity() { }
+  }
 }
-    
